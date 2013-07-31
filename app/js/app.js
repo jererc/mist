57ec81f1a50294521062040457f4f15c0c20c52a
Jerome Clerc <jererc@gmail.com>
2013-07-31 23:27:52 +0200

    default page

--- app/js/app.js
+++ app/js/app.js
@@ -3,10 +3,10 @@
 angular.module('mistApp', ['mistServices', 'mistDirectives', 'mistFilters', 'angularUtils']).
     config(['$routeProvider', function($routeProvider) {
     $routeProvider.
-        when('/', {redirectTo: '/hosts'}).
+        when('/', {redirectTo: '/syncs'}).
+        when('/syncs', {templateUrl: 'partials/sync-list.html', controller: SyncListCtrl}).
         when('/hosts', {templateUrl: 'partials/host-list.html', controller: HostListCtrl}).
         when('/users', {templateUrl: 'partials/user-list.html', controller: UserListCtrl}).
-        when('/syncs', {templateUrl: 'partials/sync-list.html', controller: SyncListCtrl}).
         when('/settings', {templateUrl: 'partials/settings-list.html', controller: SettingsListCtrl}).
         otherwise({redirectTo: '/'});
 }]);
